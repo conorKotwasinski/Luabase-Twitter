26d35b7f7d34498d7c96553d40d7f1cb417878ab
steven-luabase <steven-luabase@Stevens-MacBook-Pro-3.local>
2022-05-24 11:49:01 -0400

    changed url from localhost to cloud job url

--- app.py
+++ app.py
@@ -290,8 +290,8 @@ def get_jobs():
         pgu.updateJobStatus(db.engine, updateJobRow)
         # send job to cloud run with post request
         # url = "https://luabase-mjr-py.ngrok.io/run_job"
-        # url = "https://luabase-py-msgn5tdnsa-uc.a.run.app/run_job"
-        url = "http://localhost:5000/run_job"
+        # url = "http://localhost:5000/run_job"
+        url = "https://luabase-py-msgn5tdnsa-uc.a.run.app/run_job"
         payload = job['details']
         payload['id'] = job['id']
         headers = {"content-type": "application/json"}

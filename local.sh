af629c6d5205a5c95029f2aa5191504bb7f2e1fc
mike-luabase <mike@luabase.com>
2022-05-25 15:48:01 -0400

    added extra_data in logging4

--- local.sh
+++ local.sh
@@ -3,6 +3,7 @@ source ~/.zprofile
 
 export DOCKER_TAG=luabase-py
 export PORT=5000
+export RUNNING_LOCAL=1
 
 echo "starting build..."
 
@@ -13,6 +14,7 @@ docker build -t ${DOCKER_TAG} .
 docker run -it \
     -p ${PORT}:${PORT} \
     --env-file .env \
+    -e RUNNING_LOCAL=${RUNNING_LOCAL} \
     -e GOOGLE_APPLICATION_CREDENTIALS=/app/luabase-dev.json \
     -v $GOOGLE_APPLICATION_CREDENTIALS:/app/luabase-dev.json:ro \
     ${DOCKER_TAG}:latest
